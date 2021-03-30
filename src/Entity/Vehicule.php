525dffa896d2c27cdcff75798734dd910e0ecf0d
Amandine <amandinebensekhri@MacBook-Air-de-Amandine.local>
2021-03-30 17:06:00 +0200

    creation bdd horiza et table vehicule/utilisateur

---
+++ src/Entity/Vehicule.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VehiculeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VehiculeRepository::class)
+ */
+class Vehicule
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $immatriculation;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $marque;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $modele;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $couleur;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $img1;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $img2;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre_place;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $carburant;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImmatriculation(): ?string
+    {
+        return $this->immatriculation;
+    }
+
+    public function setImmatriculation(string $immatriculation): self
+    {
+        $this->immatriculation = $immatriculation;
+
+        return $this;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(string $marque): self
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(string $modele): self
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    public function getCouleur(): ?string
+    {
+        return $this->couleur;
+    }
+
+    public function setCouleur(string $couleur): self
+    {
+        $this->couleur = $couleur;
+
+        return $this;
+    }
+
+    public function getImg1(): ?string
+    {
+        return $this->img1;
+    }
+
+    public function setImg1(string $img1): self
+    {
+        $this->img1 = $img1;
+
+        return $this;
+    }
+
+    public function getImg2(): ?string
+    {
+        return $this->img2;
+    }
+
+    public function setImg2(?string $img2): self
+    {
+        $this->img2 = $img2;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(string $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getNombrePlace(): ?string
+    {
+        return $this->nombre_place;
+    }
+
+    public function setNombrePlace(string $nombre_place): self
+    {
+        $this->nombre_place = $nombre_place;
+
+        return $this;
+    }
+
+    public function getCarburant(): ?string
+    {
+        return $this->carburant;
+    }
+
+    public function setCarburant(string $carburant): self
+    {
+        $this->carburant = $carburant;
+
+        return $this;
+    }
+}
