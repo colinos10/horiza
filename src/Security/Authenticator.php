81900db166f2aa67df4e4af22de51e19b036ec51
Amandine <amandinebensekhri@MacBook-Air-de-Amandine.local>
2021-04-02 17:51:10 +0200

    creation controller profil (mon compte)

--- src/Security/Authenticator.php
+++ src/Security/Authenticator.php
@@ -96,7 +96,7 @@ class Authenticator extends AbstractFormLoginAuthenticator implements PasswordAu
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('admin'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
