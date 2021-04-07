33edc73053a7a1fcf75b4acf0b59ea77579b5de5
Amandine <amandinebensekhri@MacBook-Air-de-Amandine.local>
2021-04-07 14:14:17 +0200

    màj formulaire User

---
+++ src/Form/ProfileType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('password')
+            ->add('nom')
+            ->add('prenom')
+            ->add('tel')
+            ->add('img1')
+            ->add('civilite')
+            ->add('adresse')
+            ->add('code_postal')
+            ->add('ville')
+            ->add('date_naissance')
+            ->add('valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
