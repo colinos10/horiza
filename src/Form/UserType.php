ac6f7711f95cc2fb911157e119625fc6c379e209
Amandine <amandinebensekhri@MacBook-Air-de-Amandine.local>
2021-04-07 12:41:11 +0200

    création controller User + création formulaire User

---
+++ src/Form/UserType.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'prenom.nom@domaine.com'
+                ]
+            ])
+            ->add('roles', ChoiceType::class, [
+                'required' => false,
+                'multiple' => true,
+                'expanded' =>true,
+                'choices' => [
+                    'super admin' => 'ROLE_SUPER_ADMIN',
+                    'administrateur' => 'ROLE_ADMIN',
+                    'utilisateur' => 'ROLE_USER'
+                ]
+            ])
+            ->add('password')
+            ->add('nom')
+            ->add('prenom')
+            ->add('tel')
+            ->add('img1', FileType::class, [
+                'required' => false,
+                'label' => 'Photo principale',
+                'mapped' => false,
+                'help' => 'png, jpg, jpeg ou jp2 - 1 Mo maximum',
+                'constraints' => [
+                    new Image([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpg',
+                            'image/jpeg',
+                            'image/jp2'
+                        ],
+                        'mimeTypesMessage' => 'Merci de sélectionner une image au format PNG, JPG, JPEG ou JP2'
+                    ])
+                ]
+            ])
+            ->add('civilite')
+            ->add('adresse')
+            ->add('code_postal')
+            ->add('ville')
+            ->add('date_naissance')
+            ->add('valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
