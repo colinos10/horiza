30fbb36d9db759cf0346860339f0446cef21d7aa
Amandine <amandinebensekhri@MacBook-Air-de-Amandine.local>
2021-04-08 09:57:44 +0200

    màj route page A propos de nous (about)

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -7,8 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AboutController extends AbstractController
-{
-    #[Route('/about', name: 'about')]
+{   
+    /** 
+    * @Route("/about", name="about")
+    */
     public function index(): Response
     {
         return $this->render('home/about.html.twig', [
--- src/Security/Authenticator.php
+++ src/Security/Authenticator.php
@@ -95,7 +95,7 @@ class Authenticator extends AbstractFormLoginAuthenticator implements PasswordAu
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
-
+        
         return new RedirectResponse($this->urlGenerator->generate('home'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
