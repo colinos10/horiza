bc2e1c6a81f120b52691ad6fc582776613e13a4d
Amandine <amandinebensekhri@MacBook-Air-de-Amandine.local>
2021-04-08 10:03:39 +0200

    màj route page CGU CGV

--- src/Controller/CguController.php
+++ src/Controller/CguController.php
@@ -7,8 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CguController extends AbstractController
-{
-    #[Route('/cgu-cgv', name: 'cgu')]
+{   
+    /** 
+    * @Route("/cgu-cgv", name="cgu")
+    */
     public function index(): Response
     {
         return $this->render('home/cgu-cgv.html.twig', [
